06bf63f5285a0459deb6884af97159ebe868346d
SalzTheBerg <silas.hittler@gmx.de>
2025-04-24 10:04:40 +0200

    Check if os is linux for mvn path

--- src/global_parameters.py
+++ src/global_parameters.py
@@ -1,4 +1,5 @@
 import os
+import platform
 ## constant parameters
 # directory to store cloned repository
 clone_dir = "cloned_repository"
@@ -9,7 +10,10 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 # path to mvn cli
 #mvn_path = "mvn.cmd"
 # TODO Change to relative path
-mvn_path = r"F:\[21] Master\[P50] Projektarbeit 1\apache-maven-3.9.9\bin\mvn.cmd"
+if platform.system() == "Windows":
+    mvn_path = r"F:\[21] Master\[P50] Projektarbeit 1\apache-maven-3.9.9\bin\mvn.cmd"
+else:
+    mvn_path = "mvn"
 
 ## variable / temporary parameters
 # URL of github repository to clone
