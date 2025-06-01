daac1f17ff7c40f7d8d91e6ee9377850c5f76f73
SalzTheBerg <silas.hittler@gmx.de>
2025-06-01 22:42:54 +0200

    Finalized graph creation for feedback

--- src/gephi_util.py
+++ src/gephi_util.py
@@ -22,24 +22,26 @@ def export_db_to_gexf():
     cursor.execute(f'SELECT version, date FROM "{repo_name}" ORDER BY date DESC')
     rows = cursor.fetchall()
     previous = None
+    id = 0
     for version, date in rows:
-        node_name = version + " - " + date
-        G.add_node(node_name, color='red')
-        if previous:
-            G.add_edge(previous, node_name)
+        G.add_node(id, viz={'color': {'r': 255, 'g': 0, 'b': 0, 'a': 1.0}}, version=version, date=date, group="main", parent=id)
+        if previous is not None:
+            G.add_edge(previous, id)
         cursor.execute(f'''SELECT * FROM "{repo_name}" WHERE version = ?''', [version])
         row = cursor.fetchone()
         columns = [desc[0] for desc in cursor.description]
+
+        previous = id
+        id += 1
 
         for col_name, value in zip(columns, row):
             # ignore version and date columns
             if col_name in ('VERSION', 'DATE'):
                 continue
             if value is not None:
-                G.add_node(f"{col_name}:{value}")
-                G.add_edge(node_name, f"{col_name}:{value}")
-
-        previous = node_name
+                G.add_node(id, dependency=col_name, dependency_version=value, group="dependency", parent=previous)
+                G.add_edge(previous, id)
+                id += 1
 
     nx.write_gexf(G, gephi_path)
 
