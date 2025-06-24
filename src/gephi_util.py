27c64409d49e16d74898d533b11d20a371b49fa0
SalzTheBerg <silas.hittler@gmx.de>
2025-06-24 16:14:36 +0200

    Added manual graph position

--- src/gephi_util.py
+++ src/gephi_util.py
@@ -1,6 +1,7 @@
 import jaydebeapi
 from global_parameters import *
 import networkx as nx
+import math
 
 def export_db_to_gexf():
      # jdbc url for h2 db
@@ -22,9 +23,21 @@ def export_db_to_gexf():
     cursor.execute(f'SELECT version, date FROM "{repo_name}" ORDER BY date DESC')
     rows = cursor.fetchall()
     previous = None
+    spacing = 800
+    x_position = 0
+    y_position = 0
     id = 0
     for version, date in rows:
-        G.add_node(id, viz={'color': {'r': 255, 'g': 0, 'b': 0, 'a': 1.0}}, version=version, date=date, group="main", parent=id)
+        G.add_node(id,
+                   label=f"{version}",
+                   viz={
+                       'color': {'r': 255, 'g': 0, 'b': 0, 'a': 1.0},
+                       'position': {'x': x_position, 'y': y_position, 'z': 0}
+                       },
+                   version=version,
+                   date=date,
+                   group="main",
+                   parent=id)
         if previous is not None:
             G.add_edge(previous, id)
         cursor.execute(f'''SELECT * FROM "{repo_name}" WHERE version = ?''', [version])
@@ -34,14 +47,40 @@ def export_db_to_gexf():
         previous = id
         id += 1
 
+
+        radius = 200
+        num_dependencies = len([v for v in row if v is not None]) - 2
+        angle_step = 2 * math.pi / max(num_dependencies, 1)
+
+        dep_index = 0
+
         for col_name, value in zip(columns, row):
-            # ignore version and date columns
             if col_name in ('VERSION', 'DATE'):
                 continue
             if value is not None:
-                G.add_node(id, dependency=col_name, dependency_version=value, group="dependency", parent=previous)
+                angle = (dep_index * angle_step) + math.pi /2
+                x_offset = radius * math.cos(angle)
+                y_offset = radius * math.sin(angle)
+                
+                parent_pos = G.nodes[previous].get('viz', {}).get('position', {'x': 0, 'y': 0})
+                x = parent_pos['x'] + x_offset
+                y = parent_pos['y'] + y_offset
+
+                G.add_node(
+                    id,
+                    label=f"{col_name} {value}",
+                    dependency=col_name,
+                    dependency_version=value,
+                    group="dependency",
+                    parent=previous,
+                    viz={'position': {'x': x, 'y': y, 'z': 0}}
+                )
                 G.add_edge(previous, id)
                 id += 1
+                dep_index += 1
+
+
+        x_position += spacing
 
     nx.write_gexf(G, gephi_path)
 
